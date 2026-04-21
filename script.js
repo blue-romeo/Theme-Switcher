abef2f6b9002804d2c44624383e2fc23b94bbb0e
blue-romeo <romeoangasa@gmail.com>
2026-04-21 20:29:18 +0300

    functionality added

---
+++ script.js
@@ -0,0 +1,10 @@
+const themes = [
+    {name:"dark",message:"This is Dark Mode"},
+    {name:"moody",message:"This is Moody Mode"}
+];
+
+const btn = document.getElementById("theme-switcher-button");
+const dropdown = document.getElementById("theme-dropdown");
+btn.addEventListener("click", ()=> {
+
+})
